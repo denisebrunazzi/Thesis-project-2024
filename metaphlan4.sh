fddc9cda27023244e71834014f20364a14cdc5b4
Denise Brunazzi <128818917+denisebrunazzi@users.noreply.github.com>
2024-08-02 14:51:36 +0200

    Create metaphlan4.sh

---
+++ metaphlan4.sh
@@ -0,0 +1,17 @@
+conda activate metaphlan4
+
+mkdir metaphlan/
+
+# Generates a .tsv file containing the abundances of specific species and strains inside the sample.
+for i in $(cut -f7 PRJNA561510_tsv.txt | sed 1d | cut -f1 -d\; | cut -f6 -d/)  # PRJNA561510_tsv.txt contains the FASTA sequences
+do
+metaphlan sequences/${i}_1.fastq,sequences/${i}_2.fastq \
+--nproc 10 \   # Uses 10 processing to be faster
+--input_type fastq \
+-o metaphlan/${i}_metagenome.tsv \
+-s metaphlan/${i}_metagenome.sam \
+--bowtie2out metaphlan/${i}.bowtie2.bz2  # specify this when you have multiple sequences to peed up the procedure
+done
+
+# The .tsv files are merged together in a table containing the sequence reads in the columns, and all the identified bacterial species and subspecies in the rows.
+merge_metaphlan_tables.py SRR125577*.tsv > Wibowo_metaphlan4.tsv
